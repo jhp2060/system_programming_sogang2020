3d8329bb29a798e660261dce334b7648f03188b2
jhp2060 <jhp2060@gmail.com>
2020-04-02 10:31:10 -0700

    refactor(main) tokenizers

--- sp20140825_proj1/20140825.c
+++ sp20140825_proj1/20140825.c
@@ -1,13 +1,14 @@
 #include "20140825.h"
 
-char input[100];
-
 int main() {
     init();
     command ret;
     while(1) {
 	printf("sicsim> ");
-	scanf("%s", input);
+	scanf("%[^\n]", input);
+	getchar();
+	flush_tokens();
+	tokenize_input();
 	ret = get_command();
 	if (ret != _none) {
 	    if (ret == _quit) break;
@@ -19,11 +20,45 @@ int main() {
     return 0; 
 }
 
+// flush the tokens before getting new tokens
+void flush_tokens(void) {
+    int i;
+    for (i = 0; i < MAX_TOKENS; i++) strcpy(tokens[i], "\0");
+}
+
+// return the next token's first index in input
+int get_next_token_idx(char* str) {
+    int i = 0;
+    if (str[i] == '\0') return -1; 			// no more token
+    while (str[i] != ' ' && str[i] != '\0') i++;	// get the next token's first idx
+    if (str[i] == '\0') return i;
+    str[i++] = '\0'; 	                                // separate the tokens using a null char
+    while (str[i] == ' ') i++;
+    return i;
+}
+
+// parse the command and arguments from input, into tokens
+void tokenize_input(void) {
+    int start_idx = 0, next_idx = 0, token_cnt = 0;
+    
+    while (input[start_idx] == ' ') start_idx++;		// remove whitespaces in left
+    while (start_idx < MAX_INPUT_LEN) {
+	next_idx = get_next_token_idx(input + start_idx);
+	if (next_idx == -1) break;				// no more tokens
+	if (token_cnt >= MAX_TOKENS) break;			// check the limitations
+	if (strlen(input + start_idx) > MAX_TOKEN_LEN) break;
+	strcpy(tokens[token_cnt++], input + start_idx);   	// store the tokens
+	start_idx += next_idx;
+    }
+}
+
 command get_command(void) {
-    if (strcmp_twice(input, "h", "help")) return _help;
-    else if (strcmp_twice(input, "q", "quit")) return _quit;
-    else if (strcmp_twice(input, "d", "dir")) return _dir;
-    else if (strcmp_twice(input, "hi", "history")) return _history;
+    char* cmd = tokens[0];
+    if (strcmp_twice(cmd, "h", "help")) return _help;
+    else if (strcmp_twice(cmd, "q", "quit")) return _quit;
+    else if (strcmp_twice(cmd, "d", "dir")) return _dir;
+    else if (strcmp_twice(cmd, "hi", "history")) return _history;
+    else if (strcmp_twice(cmd, "du", "dump")) return _dump;
     return _none;
 }
 
@@ -41,6 +76,9 @@ void execute_instructions(command c) {
     case _history:
 	history();
 	break;
+    case _dump:
+        dump(0, 16*10);
+        break;
     default:
 	break;
     }
@@ -49,6 +87,7 @@ void execute_instructions(command c) {
 void init(void) {
     HEAD_LOG = NULL;
     TAIL_LOG = NULL;
+    LAST_ADDR = 0;
 }
 
 void exit_program(void) {
--- sp20140825_proj1/20140825.h
+++ sp20140825_proj1/20140825.h
@@ -1,3 +1,6 @@
+#ifndef __20140825_H__
+#define __20140825_H__
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,6 +8,13 @@
 #include "helpers.h"
 #include "shell_commands.h"
 #include "memory_commands.h"
+
+#define MAX_INPUT_LEN 100
+#define MAX_TOKEN_LEN 20
+#define MAX_TOKENS 10
+
+char input[MAX_INPUT_LEN];
+char tokens[MAX_TOKENS][MAX_TOKEN_LEN];
 
 typedef enum _command {
     _help=0,
@@ -20,7 +30,13 @@ typedef enum _command {
     _none
 }command;
 
+void flush_tokens(void);
+int get_next_token_idx(char* str);
+void tokenize_input(void);
+
 command get_command(void);
 void execute_instructions(command c);
 void init(void);
 void exit_program(void);
+
+#endif
