c302818a885bf0e7a8d72cf6a4d347e4e5a88375
jhp2060 <jhp2060@gmail.com>
2020-04-02 08:40:28 -0700

    feat(command) updated header for memory commands

--- sp20140825_proj1/memory_commands.h
+++ sp20140825_proj1/memory_commands.h
@@ -4,7 +4,12 @@
 #include <stdio.h>
 
 char MEM[65536][16];
+int LAST_ADDR = 0;
 
 void dump(int start, int end);
+
+void print_address(int row);
+void print_bytes(int row, int st, int ed);
+void print_chars(int row);
 
 #endif
