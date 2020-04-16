01f4da32a05a5ef09192ba31b953436dd82d34d8
jhp2060 <jhp2060@gmail.com>
2020-04-16 02:01:36 -0700

    feat(link) makefile for assembler_commands

---
+++ sp20140825_proj2/assembler_commands.c
@@ -0,0 +1,12 @@
+#include "assembler_commands.h"
+
+error assemble(char* filename, int token_count) {
+    if (token_count != 2) return ERR_WRONG_TOKENS;
+    FILE* fp = fopen(filename, "r");
+    if (!fp) return ERR_NOT_A_FILE;
+    int len = strlen(filename);
+    if (len < 5 || strcmp(filename + len - 4, ".asm"))
+	return ERR_NOT_A_ASM_FILE;  
+    fclose(fp);
+    return NO_ERR;
+}
---
+++ sp20140825_proj2/assembler_commands.h
@@ -0,0 +1,10 @@
+#ifndef __ASSEMBLE_COMMANDS_H__
+#define __ASSEMBLE_COMMANDS_H__
+
+#include "helpers.h"
+#include <string.h>
+#include <stdio.h>
+
+error assemble(char* filename, int token_count);
+
+#endif
